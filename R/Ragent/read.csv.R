a6fe3bcb5828be166c861f6d85f1fdd72742fae3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-17 14:15:24 +0800

    Update read.csv.R

--- R/Ragent/read.csv.R
+++ R/Ragent/read.csv.R
@@ -1,3 +1,13 @@
-const agent_readcsv = function() {
-    
+#' function for read csv table
+#' 
+#' @param file the filepath of the csv table file
+#' 
+#' @return this function returns a json encode text of the csv data,
+#'   csv data is encoded as the row json
+#' 
+[@ollama "read_csv"]
+const agent_readcsv = function(file) {
+    let data = read.csv(file, row.names = 1, check.names = FALSE);
+    data = as.list(data,byrow=TRUE);
+    JSON::json_encode(data);
 }
