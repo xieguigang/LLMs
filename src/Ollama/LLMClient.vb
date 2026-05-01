5c0714dd57359c3a33d5d6aea0a4dbaf934c2dae
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-01 11:14:48 +0800

    Update LLMClient.vb

--- src/Ollama/LLMClient.vb
+++ src/Ollama/LLMClient.vb
@@ -13,7 +13,7 @@ Public Class LLMClient : Implements IDisposable
     Dim ai_caller As New FunctionCaller
     Dim ai_log As TextWriter
     Dim ai_calls As New List(Of FunctionCall)
-
+    Private disposedValue As Boolean
     Public Property temperature As Double = 0.1
     Public Property tools As List(Of FunctionTool)
 
@@ -96,4 +96,28 @@ Public Class LLMClient : Implements IDisposable
         Throw New Exception("Exceeded max tool call rounds")
     End Function
 
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
 End Class
