1c08e53aeb3d0afcdc49b3fd26fd8ba13b60c0ac
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-29 17:35:19 +0800

    implements of the webview host message

--- src/WebView2UI/WebView2LLMUI.vb
+++ src/WebView2UI/WebView2LLMUI.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.InteropServices
 Imports System.Text.Json
+Imports System.Threading
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.Web.WebView2.Core
 Imports Ollama
@@ -17,24 +18,112 @@ Public Class WebView2LLMUI
         End Sub
 
         ''' <summary>
-        ''' javascript call host method for send prompt text to llm
+        ''' the current model name that configured in the host llm client, use for display in the web ui
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property Model As String
+            Get
+                If host.llm_host Is Nothing Then
+                    Return ""
+                Else
+                    Return host.llm_host.Model
+                End If
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' javascript call host method for send prompt text to llm, the response will be streamed
+        ''' back to the web page via the push_token / start_response / end_response web messages.
         ''' </summary>
         ''' <param name="prompt_text"></param>
-        ''' <returns></returns>
         Public Async Function SendMessage(prompt_text As String) As Task(Of String)
-            If host.llm_host IsNot Nothing Then
-                Return (Await host.llm_host.Chat(prompt_text)).GetJson
-            Else
+            If host.llm_host Is Nothing Then
+                Call host.PushError("the llm host is not configured, please call SetHost first.")
                 Return (New LLMsResponse).GetJson
             End If
+
+            Call host.BeginChat()
+
+            Try
+                Dim response = Await host.llm_host.Chat(prompt_text, host._cts.Token)
+
+                If response Is Nothing Then
+                    Call host.PushEnd("", "")
+                Else
+                    Call host.PushEnd(response.output, response.think)
+                    Return response.GetJson
+                End If
+            Catch ex As OperationCanceledException
+                ' user cancelled the generation, just end the response quietly
+                Call host.PushEnd("", "")
+            Catch ex As Exception
+                Call host.PushError(ex.Message)
+            End Try
+
+            Return (New LLMsResponse).GetJson
         End Function
+
+        ''' <summary>
+        ''' cancel the current llm generation, triggered from the web ui stop button
+        ''' </summary>
+        Public Sub StopGeneration()
+            Call host.StopChat()
+        End Sub
+
+        ''' <summary>
+        ''' clear the conversation history in the host llm client and reset the web ui message list
+        ''' </summary>
+        Public Sub ResetConversation()
+            Call host.ResetConversation()
+        End Sub
 
     End Class
 
     Dim llm_host As LLMClient
+    Dim _cts As CancellationTokenSource
 
+    ''' <summary>
+    ''' bind a <see cref="LLMClient"/> instance as the backend of this chat control. the response
+    ''' stream hook will be enabled so that the think/output tokens can be pushed to the web ui.
+    ''' </summary>
+    ''' <param name="llm_host"></param>
     Public Sub SetHost(llm_host As LLMClient)
         Me.llm_host = llm_host.HookResponseStream(getOutputToken:=AddressOf PushOutputToken, getThinkToken:=AddressOf PushThinkToken)
+    End Sub
+
+    ''' <summary>
+    ''' start a new chat response: create a fresh cancellation token and notify the web ui to
+    ''' create a new assistant bubble.
+    ''' </summary>
+    Private Sub BeginChat()
+        If _cts IsNot Nothing Then
+            Call _cts.Dispose()
+        End If
+
+        _cts = New CancellationTokenSource()
+        Call PushStart()
+    End Sub
+
+    ''' <summary>
+    ''' cancel the current chat generation
+    ''' </summary>
+    Private Sub StopChat()
+        If _cts IsNot Nothing Then
+            Call _cts.Cancel()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' clear conversation memory in host and reset the web ui message list
+    ''' </summary>
+    Private Sub ResetConversation()
+        If llm_host IsNot Nothing Then
+            Call llm_host.Clear()
+        End If
+
+        Call SendMessage(New With {
+            .action = "reset"
+        })
     End Sub
 
     ''' <summary>
@@ -61,8 +150,54 @@ Public Class WebView2LLMUI
         })
     End Sub
 
+    ''' <summary>
+    ''' notify the web ui that a new assistant response starts
+    ''' </summary>
+    Public Sub PushStart()
+        Call SendMessage(New With {
+            .action = "start_response",
+            .role = "assistant"
+        })
+    End Sub
+
+    ''' <summary>
+    ''' notify the web ui that the current assistant response ends, the final text is used as a
+    ''' safe fallback in case some streamed tokens were missed.
+    ''' </summary>
+    Public Sub PushEnd(output As String, think As String)
+        Call SendMessage(New With {
+            .action = "end_response",
+            .output = output,
+            .think = think
+        })
+    End Sub
+
+    ''' <summary>
+    ''' notify the web ui that an error occured during the chat
+    ''' </summary>
+    Public Sub PushError(err As String)
+        Call SendMessage(New With {
+            .action = "error",
+            .message = err
+        })
+    End Sub
+
+    ''' <summary>
+    ''' post a json payload from the host to the web html ui, the call is marshaled to the UI
+    ''' thread to keep CoreWebView2 happy when the token callback fires on a background thread.
+    ''' </summary>
     Private Sub SendMessage(Of T As Class)(payload As T)
-        WebView21.CoreWebView2.PostWebMessageAsJson(JsonSerializer.Serialize(payload))
+        If WebView21 Is Nothing OrElse WebView21.CoreWebView2 Is Nothing Then
+            Return
+        End If
+
+        Dim json = JsonSerializer.Serialize(payload)
+
+        If WebView21.InvokeRequired Then
+            WebView21.Invoke(Sub() WebView21.CoreWebView2.PostWebMessageAsJson(json))
+        Else
+            WebView21.CoreWebView2.PostWebMessageAsJson(json)
+        End If
     End Sub
 
     Private Async Sub WebView2LLMUI_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -73,4 +208,5 @@ Public Class WebView2LLMUI
         WebView21.CoreWebView2.AddHostObjectToScript("llm_host", New LLMHost(Me))
         WebViewLoader.NavigateToLargeString(WebView21, HtmlUiResource.index)
     End Sub
+
 End Class
