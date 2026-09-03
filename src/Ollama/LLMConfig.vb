15ba99082fee7ccea6c554a471ef322da09370ba
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-04 03:15:24 +0800

    llm config data

---
+++ src/Ollama/LLMConfig.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Settings.Inf
+
+Public Class LLMConfig
+
+    Public Property provider As String
+    Public Property key As String
+    Public Property model As String
+
+    Public Function CreateLLm() As LLMClient
+        Return New LLMClient(LLMUrl.Create(provider, apikey:=key), model)
+    End Function
+
+    Public Sub Save(ini As String)
+        Using inifile As New IniFile(ini)
+            Dim data = inifile.OpenSection("llm")
+
+            Call data.SetValue("provider", provider)
+            Call data.SetValue("key", key)
+            Call data.SetValue("model", model)
+        End Using
+    End Sub
+
+    Public Shared Function Load(ini As String) As LLMConfig
+        Dim inifile As New IniFile(ini)
+        Dim config As Section = inifile.OpenSection("llm")
+        Dim llm As New LLMConfig With {
+            .key = config.GetValue("key"),
+            .model = config.GetValue("model"),
+            .provider = config.GetValue("provider")
+        }
+
+        Return llm
+    End Function
+
+End Class
